a3cef05a7cd18179535f337f814051c6d5c4f23c
KriZa96 <kriza9605@gmail.com>
2024-10-21 20:08:37 +0200

    Task done.

--- app/app.cpp
+++ app/app.cpp
@@ -6,6 +6,68 @@ namespace vsite::oop::v2
 /*
 * class member function implementations
 */
+	double color::get_red() const
+	{
+		return this->red;
+	}
+	double color::get_blue() const
+	{
+		return this->blue;
+	}
+	double color::get_green() const
+	{
+		return this->green;
+	}
+	void color::set_red(const double num)
+	{
+		if (num < 0)
+		{
+			this->red = 0;
+			return;
+		}
+		if (num > 1)
+		{
+			this->red = 1;
+			return;
+		}
+		this->red = num;
+	}
+	void color::set_blue(const double num)
+	{
+		if (num < 0)
+		{
+			this->blue = 0;
+			return;
+		}
+		if (num > 1)
+		{
+			this->blue = 1;
+			return;
+		}
+		this->blue = num;
+	}
+	void color::set_green(const double num)
+	{
+		if (num < 0)
+		{
+			this->green = 0;
+			return;
+		}
+		if (num > 1)
+		{
+			this->green = 1;
+			return;
+		}
+		this->green = num;
+	}
+	double color::get_luminance() const
+	{
+		return (0.2126 * this->red + 0.7152 * this->green + 0.0722 * this->blue);
+	}
+	uint32_t color::get_color_ref() const
+	{
+		return RGB(this->red * 255, this->green * 255, this->blue * 255);
+	}
 
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -9,6 +9,23 @@ namespace vsite::oop::v2
 /*
 * class declaration
 */
+	class color
+	{
+	public:
+		double get_red() const;
+		double get_blue() const;
+		double get_green() const;
+		double get_luminance() const;
+		uint32_t get_color_ref() const;
+		void set_red(const double num);
+		void set_blue(const double num);
+		void set_green(const double num);
+	private:
+		double red = 0;
+		double blue = 0;
+		double green = 0;
+	};
+
 
 
 }
